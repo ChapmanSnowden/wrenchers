16fc6780f033115075c0dd83711b0d9f4e602feb
Chapman Snowden <Chapman.Snowden@gmail.com>
2013-12-08 19:36:17 -0800

    maiden commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wrenchers::Application.config.secret_key_base = '2400829962aa3be584f1977f39c218ba73576548baf3d2e99876df9e36751028d89fd73a9bac918f88bc96abc8c47461817c2c0f23fbc8c76dcd461b671ae720'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Wrenchers::Application.config.session_store :cookie_store, key: '_wrenchers_session'
